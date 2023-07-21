0f6505918562ade28a425ab2052ee5dd83ffc602
tt954 <61483705+tt954@users.noreply.github.com>
2023-07-20 21:00:35 -0400

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
+import { useState } from "react";
+
 import { GradientBackground } from "./components/Background";
 import {
   FixedBackgroundImage,
   RelativeBackgroundImage,
 } from "./components/Image";
+import { Footer, FooterLink } from "./components/Footer";
 
 import Cloud from "./assets/cloud.png";
 import Moon from "./assets/moon.png";
 
 export default function Home() {
+  const [numQuotes, setNumQuotes] = useState<Number | null>(0);
+
   return (
     <main>
       <GradientBackground>
@@ -21,8 +26,23 @@ export default function Home() {
         <FixedBackgroundImage
           src={Moon}
           alt="moon from vecteezy"
-          height="300"
+          height="500"
         />
+
+        <Footer>
+          <>
+            total quotes generated: {numQuotes}
+            <br />
+            developed with ♥ by{" "}
+            <FooterLink
+              href="https://github.com/tt954"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @tt954
+            </FooterLink>
+          </>
+        </Footer>
       </GradientBackground>
     </main>
   );
