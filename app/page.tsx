4df9a6ff8bbc3354121c3176bd9c18ef4755e8bc
tt954 <61483705+tt954@users.noreply.github.com>
2023-08-08 10:50:46 -0400

    add updateNumQuote to query for numQuotes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,49 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classnames from "classnames";
+import { API } from "aws-amplify";
 
 // import Cloud from "./assets/cloud.png";
 // import Moon from "./assets/moon.png";
 
-import styles from "./styles/page.module.css";
+import styles from "@/styles/page.module.css";
+import { quoteQueryName } from "@/src/graphql/queries";
+
+// interface for DynamoDB object
+interface UpdateQuoteData {
+  id: string;
+  queryName: string;
+  quotesGenerated: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// type guard for fetch function
 
 export default function Home() {
   const [numQuotes, setNumQuotes] = useState<Number>(0);
   const [showOverlay, setShowOverlay] = useState<Boolean>(false);
+
+  // fetch DynamoDB object for number of quotes generated
+  const updateNumQuote = async () => {
+    try {
+      const response = await API.graphql<UpdateQuoteData>({
+        query: quoteQueryName,
+        authMode: "AWS_IAM",
+        variables: {
+          queryName: "numQuotes",
+        },
+      });
+      console.log("response :>> ", response);
+    } catch (error) {
+      console.log("Error getting number of quotes generated :>> ", error);
+    }
+  };
+
+  useEffect(() => {
+    updateNumQuote();
+  }, []);
 
   return (
     <main>
